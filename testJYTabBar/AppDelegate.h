39237f7a8816e0db61ec45dca7abc9300b9f4c26
@jiqiaochun <13011251196@163.com>
2016-04-22 09:45:32 +0800

    Initial Commit

---
+++ testJYTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testJYTabBar
+//
+//  Created by 姬巧春 on 16/4/22.
+//  Copyright © 2016年 姬巧春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testJYTabBar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testJYTabBar
+//
+//  Created by 姬巧春 on 16/4/22.
+//  Copyright © 2016年 姬巧春. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
